bae262caca9bcc74ef8bd621ff89f79856f099a4
Ansh Sikka <sikka008@umn.edu>
2021-12-15 23:06:36 -0600

    Making some changes

--- src/App.js
+++ src/App.js
@@ -5,35 +5,34 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Menu } from "semantic-ui-react";
 import "./App.css";
 
-
+function Nav() {
+  return (
+    <Menu className="Menu" secondary>
+      <a href="/home">
+        <Menu.Item className="Item" name="Home" />
+      </a>
+      <a href="/mudstagram">
+        <Menu.Item id="Mudstagram" className="Item" name="Mudstagram" />
+      </a>
+      <a href="/mudazon">
+        <Menu.Item id="Mudazon" className="Item" name="Mudazon" />
+      </a>
+    </Menu>
+  );
+}
 function App() {
   return (
     <div className="OverlayBackground">
+      
       <BrowserRouter>
-        <Menu className="Menu" secondary>
-          <a href="/">
-          <Menu.Item className="Item"
-            name='Home'/>
-          </a>
-          <a href="/#/mudstagram">
-          <Menu.Item id="Mudstagram" className="Item"
-            name="Mudstagram"
-          />
-          </a>
-          <a href="/#/mudazon">
-          <Menu.Item id="Mudazon" className="Item"
-            name='Mudazon'
-          />
-          </a>
-
-        </Menu>
+      <Nav />
         <Routes>
-          <Route path="/" element={<Home />}/>
-          <Route path="mudstagram" element={<Mudstagram/>} />
-          <Route path="mudazon" element={<Mudazon/>} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/mudstagram" element={<Mudstagram />} />
+          <Route path="/mudazon" element={<Mudazon />} />
         </Routes>
       </BrowserRouter>
-      </div>
+    </div>
   );
 }
 
