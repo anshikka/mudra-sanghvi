6de89b9001dc0300207094823db1f9e07a65a908
Ansh Sikka <sikka008@umn.edu>
2021-12-21 23:36:21 -0600

    Removed react router

--- src/App.js
+++ src/App.js
@@ -1,39 +1,105 @@
 import Home from "./components/pages/Home";
 import Mudstagram from "./components/pages/Mudstagram";
 import Mudazon from "./components/pages/Mudazon";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Menu } from "semantic-ui-react";
+import { useState } from "react";
 import "./App.css";
 
-function Nav() {
-  return (
-    <Menu className="Menu" secondary>
-      <a href="/home">
-        <Menu.Item className="Item" name="Home" />
-      </a>
-      <a href="/mudstagram">
-        <Menu.Item id="Mudstagram" className="Item" name="Mudstagram" />
-      </a>
-      <a href="/mudazon">
-        <Menu.Item id="Mudazon" className="Item" name="Mudazon" />
-      </a>
-    </Menu>
-  );
-}
 function App() {
-  return (
-    <div className="OverlayBackground">
-      
-      <BrowserRouter>
-      <Nav />
-        <Routes>
-          <Route path="/home" element={<Home />} />
-          <Route path="/mudstagram" element={<Mudstagram />} />
-          <Route path="/mudazon" element={<Mudazon />} />
-        </Routes>
-      </BrowserRouter>
-    </div>
-  );
+  const [activeItem, setActiveItem] = useState("Home");
+
+  if (activeItem === "Home") {
+    return (
+      <div className="OverlayBackground">
+        <Menu className="Menu" secondary>
+          <Menu.Item
+            className="Item"
+            name="Home"
+            active={activeItem === "Home"}
+            onClick={() => setActiveItem("Home")}
+          />
+          <Menu.Item
+            id="Mudstagram"
+            className="Item"
+            name="Mudstagram"
+            active={activeItem === "Mudstagram"}
+            onClick={() => setActiveItem("Mudstagram")}
+          />
+
+          <Menu.Item
+            id="Mudazon"
+            className="Item"
+            name="Mudazon"
+            active={activeItem === "Mudazon"}
+            onClick={() => setActiveItem("Mudazon")}
+          />
+        </Menu>
+
+        <Home />
+      </div>
+    );
+  }
+  if (activeItem === "Mudstagram") {
+    return (
+      <div className="OverlayBackground">
+        <Menu className="Menu" secondary>
+          <Menu.Item
+            className="Item"
+            name="Home"
+            active={activeItem === "Home"}
+            onClick={() => setActiveItem("Home")}
+          />
+          <Menu.Item
+            id="Mudstagram"
+            className="Item"
+            name="Mudstagram"
+            active={activeItem === "Mudstagram"}
+            onClick={() => setActiveItem("Mudstagram")}
+          />
+
+          <Menu.Item
+            id="Mudazon"
+            className="Item"
+            name="Mudazon"
+            active={activeItem === "Mudazon"}
+            onClick={() => setActiveItem("Mudazon")}
+          />
+        </Menu>
+
+        <Mudstagram />
+      </div>
+    );
+  }
+  if (activeItem === "Mudazon") {
+    return (
+      <div className="OverlayBackground">
+        <Menu className="Menu" secondary>
+          <Menu.Item
+            className="Item"
+            name="Home"
+            active={activeItem === "Home"}
+            onClick={() => setActiveItem("Home")}
+          />
+          <Menu.Item
+            id="Mudstagram"
+            className="Item"
+            name="Mudstagram"
+            active={activeItem === "Mudstagram"}
+            onClick={() => setActiveItem("Mudstagram")}
+          />
+
+          <Menu.Item
+            id="Mudazon"
+            className="Item"
+            name="Mudazon"
+            active={activeItem === "Mudazon"}
+            onClick={() => setActiveItem("Mudazon")}
+          />
+        </Menu>
+        <Mudazon />
+      </div>
+    );
+  }
 }
 
 export default App;
